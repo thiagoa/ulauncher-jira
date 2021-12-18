b2965544b3cd898c727449b1a0eb9529abc7754d
Thiago A. Silva <thiagoaraujos@gmail.com>
2021-12-18 19:23:38 -0300

    Make extension work on latest versions

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 """ Created by Safa Arıman on 10.12.2018 """
+import gi
+gi.require_version('Gdk', '3.0')
 from jira.extension import JiraExtension
 
 __author__ = 'safaariman'
